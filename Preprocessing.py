27268228e40366a4a886edda690335f1e84bb0b4
Sayan Banerjee <59334180+sayan97@users.noreply.github.com>
2022-06-29 21:58:43 +0530

    Delete Preprocessing.py

--- Preprocessing.py
+++
@@ -1,8 +0,0 @@
-from sklearn.preprocessing import StandardScaler
-import pandas as pd
-class Preprocessor:
-    def __init__(self,data):
-        self.data=data
-
-    def to_dataframe(self):
-        df=pd.DataFrame([self.data])
